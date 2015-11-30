287b551eb880021f391a471ee63e8f7d3e0cdcda
Shine Chen <hsiehwei029@gmail.com>
2015-11-30 11:31:36 +0800

    Fix pod lint warnings.

--- Pod/Classes/QuestSDK.m
+++ Pod/Classes/QuestSDK.m
@@ -226,10 +226,10 @@ static id _sharedInstance;
 {
     NSLog(@"%s", __FUNCTION__);
     
-    [self queryPath:[NSString stringWithFormat:@"beacons/%@/%ld/%ld/flyers/",
+    [self queryPath:[NSString stringWithFormat:@"beacons/%@/%@/%@/flyers/",
                      beacon.uuid,
-                     beacon.major,
-                     beacon.minor]
+                     @(beacon.major),
+                     @(beacon.minor)]
    withSerializable:[MQFlyer class] completionHandler:complete];
 }
 
@@ -237,10 +237,10 @@ static id _sharedInstance;
 {
     NSLog(@"%s", __FUNCTION__);
     
-    [self queryPath:[NSString stringWithFormat:@"beacons/%@/%ld/%ld/stores/",
+    [self queryPath:[NSString stringWithFormat:@"beacons/%@/%@/%@/stores/",
                      beacon.uuid,
-                     beacon.major,
-                     beacon.minor]
+                     @(beacon.major),
+                     @(beacon.minor)]
    withSerializable:[MQStore class] completionHandler:complete];
 }
 
@@ -248,10 +248,10 @@ static id _sharedInstance;
 {
     NSLog(@"%s", __FUNCTION__);
     
-    [self queryPath:[NSString stringWithFormat:@"beacons/%@/%ld/%ld/notifications/",
+    [self queryPath:[NSString stringWithFormat:@"beacons/%@/%@/%@/notifications/",
                      beacon.uuid,
-                     beacon.major,
-                     beacon.minor]
+                     @(beacon.major),
+                     @(beacon.minor)]
    withSerializable:[MQNotification class] completionHandler:complete];
 }
 
