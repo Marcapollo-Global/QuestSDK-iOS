ffa2daaae482a90edb67ff707d4b8d8b7d927b25
Shine Chen <hsiehwei029@gmail.com>
2015-11-30 11:21:51 +0800

    Upgrade SDK version.

--- Pod/Classes/QuestSDK.m
+++ Pod/Classes/QuestSDK.m
@@ -26,7 +26,7 @@ const NSString *kQuestBeaconPropertyUUID = @"beacon_uuid";
 
 const NSString *kSERVER_URL = @"http://localhost:3000/v1";
 
-const NSString *kSDKVersion = @"0.1.0";
+const NSString *kSDKVersion = @"0.1.1";
 
 @interface QuestSDK() <CLLocationManagerDelegate>
 
